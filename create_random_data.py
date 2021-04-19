48b868a5561f4922f3617d97c938fcf4a4a4284d
parisaetemadinejad <parisa.etemadinejad@gmail.com>
2021-04-19 15:41:55 +0430

    initial commit

---
+++ create_random_data.py
@@ -0,0 +1,17 @@
+import random
+
+street = ["Aghdasieh", 'Azadi Avenue', 'Damavand Street', 'Doulat', 'Enqelab Street', 'Farmanieh', 'Ferdowsi Street',
+          'Imam Hossein Square', 'Jomhuri', 'Kargar Street', 'Keshavarz Boulevard',
+          'Khayyam Street', 'Laleh-Zar Street', 'Mirdamad Boulevard', 'Nelson Mandela Boulevard',
+          '(Jordan Street) Tehran', 'Nimr Baqir al-Nimr Street', 'Pasdaran (district)', 'Pasteur Street',
+          'Rah Ahan Square', 'Seoul Street', 'Shush Street', 'Sohrevardi Street', 'Surena Street', 'Tohid Tunnel',
+          'Valiasr Street', ]
+borough = [
+    'Nicole Nelson', 'Michael Baker', 'Betty Turner', 'Kevin White', 'Maria Murphy', 'Billy Scott', 'Willie Allen',
+    'Martha Mitchell',
+    'Jacqueline Jones', 'Joyce Martinez', 'Lillian Roberts', 'Julia Jenkins', 'Brian Anderson', 'Karen Evans',
+    'Angela Rivera', 'Sharon Sanchez', 'Louis Griffin', 'Mark Taylor',
+    'Marie Hughes', 'Pamela Thomas', 'Richard James', 'Ruth Watson', 'Phyllis Cooper', 'Benjamin Butler', 'Sarah Morgan'
+]
+# this code select random from list
+# print(random.choice(street))
