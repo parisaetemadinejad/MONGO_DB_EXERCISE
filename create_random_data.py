7213771f8ed2a72d654174d072b374819cb7bab7
parisaetemadinejad <parisa.etemadinejad@gmail.com>
2021-04-19 16:34:00 +0430

    initial commit

--- create_random_data.py
+++ create_random_data.py
@@ -6,12 +6,36 @@ street = ["Aghdasieh", 'Azadi Avenue', 'Damavand Street', 'Doulat', 'Enqelab Str
           '(Jordan Street) Tehran', 'Nimr Baqir al-Nimr Street', 'Pasdaran (district)', 'Pasteur Street',
           'Rah Ahan Square', 'Seoul Street', 'Shush Street', 'Sohrevardi Street', 'Surena Street', 'Tohid Tunnel',
           'Valiasr Street', ]
-borough = [
-    'Nicole Nelson', 'Michael Baker', 'Betty Turner', 'Kevin White', 'Maria Murphy', 'Billy Scott', 'Willie Allen',
-    'Martha Mitchell',
-    'Jacqueline Jones', 'Joyce Martinez', 'Lillian Roberts', 'Julia Jenkins', 'Brian Anderson', 'Karen Evans',
-    'Angela Rivera', 'Sharon Sanchez', 'Louis Griffin', 'Mark Taylor',
-    'Marie Hughes', 'Pamela Thomas', 'Richard James', 'Ruth Watson', 'Phyllis Cooper', 'Benjamin Butler', 'Sarah Morgan'
-]
+borough = ['Nicole Nelson', 'Michael Baker', 'Betty Turner', 'Kevin White', 'Maria Murphy', 'Billy Scott',
+           'Willie Allen',
+           'Martha Mitchell',
+           'Jacqueline Jones', 'Joyce Martinez', 'Lillian Roberts', 'Julia Jenkins', 'Brian Anderson', 'Karen Evans',
+           'Angela Rivera', 'Sharon Sanchez', 'Louis Griffin', 'Mark Taylor',
+           'Marie Hughes', 'Pamela Thomas', 'Richard James', 'Ruth Watson', 'Phyllis Cooper', 'Benjamin Butler',
+           'Sarah Morgan'
+
+           ]
+grade = ['A', 'B', 'C', 'D']
+
+
 # this code select random from list
 # print(random.choice(street))
+# create random zipcode and handle unique
+def zip_code(low_num, high_num, number_num):
+    set_sample = set()
+    while len(set_sample) < number_num:
+        set_sample.add(random.randint(low_num, high_num))
+    return set_sample
+
+
+def score(low_num, high_num):
+    return random.randint(low_num, high_num)
+
+
+def random_from_list(list_input):
+    return random.choice(list_input)
+
+
+# print(zip_code(1000, 3000, 1000))
+print(random_from_list(grade))
+print(score(0, 100))
